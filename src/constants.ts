8355d5abe942be236a28ff202eae6e9e603fa0cb
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-01-27 12:21:14 +0100

    Fixed a template name.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const APP_NAME = 'Sui dApp Starter'
 export const SOURCE_REPO = 'https://github.com/suiware/sui-dapp-starter'
 export const TEMPLATES = [
   { name: 'Greeting (React)', value: 'greeting-react' },
-  { name: 'Greeting (Next.js)', value: 'greeting-nextjs' },
+  { name: 'Greeting (Next.js)', value: 'greeting-next' },
   { name: 'Counter (React)', value: 'counter-react' },
 ]
 export const DEFAULT_TEMPLATE = 'greeting-react'
